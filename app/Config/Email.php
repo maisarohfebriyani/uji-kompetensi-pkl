fc2fd12f53fb62317dfa35f5cf7df102f75a8427
maisarohfebriyani <maisarohfebri@gmail.com>
2025-06-16 17:52:16 +0700

    2

--- app/Config/Email.php
+++ app/Config/Email.php
@@ -38,7 +38,7 @@ class Email extends BaseConfig
     /**
      * SMTP Password
      */
-    public string $SMTPPass = 'qvtl ebuc lhye ptyz';
+    public string $SMTPPass = '';
 
     /**
      * SMTP Port
